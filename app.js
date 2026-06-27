ed4f92e9ab8dafa9d83b443a3b312e6b26053986
abhigyansinghrajput0-sys <abhigyansinghrajput0@gmail.com>
2026-06-27 16:21:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,49 @@ const bot = new TelegramBot(token, {polling: true});
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome Bhai Abhigyan! Aapka Cloud Drive Bot active hai.");
 });
+const { google } = require("googleapis");
+const axios = require("axios");
+const streamifier = require("streamifier");
+const auth = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
+);
+
+auth.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN
+});
+
+const drive = google.drive({
+  version: "v3",
+  auth
+});
+
+bot.on("document", async (msg) => {
+  try {
+    const file = await bot.getFile(msg.document.file_id);
+
+    const url = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`;
+
+    const response = await axios.get(url, {
+      responseType: "arraybuffer"
+    });
+
+    const upload = await drive.files.create({
+      requestBody: {
+        name: msg.document.file_name
+      },
+      media: {
+        mimeType: msg.document.mime_type,
+        body: streamifier.createReadStream(Buffer.from(response.data))
+      }
+    });
+
+    bot.sendMessage(
+      msg.chat.id,
+      `✅ Upload successful!\nhttps://drive.google.com/file/d/${upload.data.id}/view`
+    );
+  } catch (err) {
+    console.log(err);
+    bot.sendMessage(msg.chat.id, "❌ Upload failed.");
+  }
+});
